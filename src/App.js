8c178e974ff46522d4940cc5e527ff644250b853
safran6ix <mohammedsafran6ix@gmail.com>
2025-07-07 17:45:33 +0530

    new style added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className="App-header">
-        <h1>Safran</h1>
-        <div className='photo'>
-              {/* phot appear here... */}
-          </div>
-      </header>
-
-      {/* <div className='container'>
-        <p>Lore</p>
-      </div> */}
-
+    <div className="landing-container">
+      <div className="landing-content">
+        <h1 className="landing-title">SAFRAN</h1>
+        <p className="landing-subtitle">
+          A futuristic development OS redefining code, collaboration, and cloud.
+        </p>
+        <a href="home.js" className="cta-button">
+          Get Early Access
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/home.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './App.js';
+
+function home() {
+    return (
+        <div className="container">
+            Hello
+        </div>
+    );
+}
+
+export default home;
