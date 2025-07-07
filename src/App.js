31503eda50b7f95f3017adcd964373ab422041e7
safran6ix <mohammedsafran6ix@gmail.com>
2025-07-07 12:45:42 +0530

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Safran</h1>
+        <div className='photo'>
+              {/* phot appear here... */}
+          </div>
       </header>
+
+      <div className='container'>
+        <p>Lore</p>
+      </div>
+
     </div>
   );
 }
