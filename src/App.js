c6336aed8a9ea85e9dcf61525635058f6719e0f7
safran6ix <mohammedsafran6ix@gmail.com>
2025-07-22 19:42:19 +0530

    Modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 function App() {
   return (
@@ -21,9 +22,15 @@ function App() {
         </p>
 
         <div className='body-cont'>
-          <div className='card'></div>
-          <div className='card'></div>
-          <div className='card'></div>
+          <div className='card'>
+            <h1>A</h1>
+          </div>
+          <div className='card'>
+            <h1>B</h1>
+          </div>
+          <div className='card'>
+            <h1>C</h1>
+          </div>
         </div>
 
       </div>
