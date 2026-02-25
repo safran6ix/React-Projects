2efc2558232b6badc72ab4bb7d29754f1db2ea7d
safran6ix <mohammedsafran6ix@gmail.com>
2026-02-26 01:28:02 +0530

    Update Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        company: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+
+}
